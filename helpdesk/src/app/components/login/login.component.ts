acdb564e421a9bda4e375485efb2704f1d83d662
Alex Costa <89145796+AlexBCosta@users.noreply.github.com>
2023-12-01 08:43:49 -0300

    Auth login configurado

--- helpdesk/src/app/components/login/login.component.ts
+++ helpdesk/src/app/components/login/login.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { FormControl, Validators } from '@angular/forms';
 import { ToastrService } from 'ngx-toastr';
 import { Credenciais } from 'src/app/models/credenciais';
+import { AuthService } from 'src/app/services/auth.service';
 
 @Component({
   selector: 'app-login',
@@ -18,7 +19,7 @@ export class LoginComponent implements OnInit {
   email = new FormControl(null, Validators.email)
   senha = new FormControl(null, Validators.minLength(3));
 
-  constructor( private toast: ToastrService) { }
+  constructor( private toast: ToastrService, private service: AuthService) { }
 
   ngOnInit(): void {
   }
@@ -32,8 +33,9 @@ export class LoginComponent implements OnInit {
   }
 
   logar(){
-    this.toast.error('Usuario ou senha inválidos!', 'Login');
-    this.creds.senha= '' 
+    this.service.authenticate(this.creds).subscribe(resposta => {
+      this.toast.info(resposta.headers.get('Authorization'))
+    })
   } 
 
 }
