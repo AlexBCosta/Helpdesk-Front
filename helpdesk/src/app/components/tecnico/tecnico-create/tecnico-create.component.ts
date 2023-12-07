fb0555e9694ed87b57a4c9b7a7a21e5bbee8aba6
Alex Costa <89145796+AlexBCosta@users.noreply.github.com>
2023-12-07 17:08:50 -0300

    Criando novo tecnico finalizado

--- helpdesk/src/app/components/tecnico/tecnico-create/tecnico-create.component.ts
+++ helpdesk/src/app/components/tecnico/tecnico-create/tecnico-create.component.ts
@@ -1,4 +1,9 @@
+import { FormControl, Validators } from '@angular/forms';
 import { Component, OnInit } from '@angular/core';
+import { TecnicoService } from 'src/app/services/tecnico.service';
+import { Tecnico } from 'src/app/models/tecnico';
+import { ToastrService } from 'ngx-toastr';
+import { Router, RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-tecnico-create',
@@ -7,9 +12,51 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TecnicoCreateComponent implements OnInit {
 
-  constructor() { }
+  tecnico: Tecnico = {
+    id:          '',
+    nome:        '',
+    email:       '',
+    cpf:         '',
+    senha:       '',
+    perfis:      [],
+    dataCriacao: ''
+  }
+
+  nome: FormControl = new FormControl(null, [Validators.minLength(3), Validators.required]);
+  cpf: FormControl = new FormControl(null, [Validators.minLength(3), Validators.required]);
+  email: FormControl = new FormControl(null, [Validators.email, Validators.required]);
+  senha: FormControl = new FormControl(null, [Validators.minLength(3), Validators.required]);
+
+  constructor(private service: TecnicoService, private toast: ToastrService, private router: Router) { }
 
   ngOnInit(): void {
   }
 
+  validarCampos(): boolean {
+    return this.nome.valid && this.cpf.valid && this.email.valid && this.senha.valid;
+  }
+
+  addPerfil(perfil: any): void {
+
+    if (this.tecnico.perfis.includes(perfil)) {
+      this.tecnico.perfis.splice(this.tecnico.perfis.indexOf(perfil), 1);
+    }else{
+      this.tecnico.perfis.push(perfil);
+    }
+  }
+
+  create(): void {
+    this.service.create(this.tecnico).subscribe(resposta => {
+    this.toast.success('Tecnico cadastrado com sucesso', 'Cadastro');
+    this.router.navigate(['tecnicos'])
+    }, excecao => {
+      if (excecao.error.errors) {
+        excecao.error.errors.forEach(element => {
+          this.toast.error(element.message);
+        });
+      }else{
+        this.toast.error(excecao.error.message);
+      }
+    })
+  }
 }
